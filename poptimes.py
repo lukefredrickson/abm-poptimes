960676d39d65136dce74ed931d633dfe5f651f54
Luke Fredrickson <lukeefredrickson@gmail.com>
2021-05-06 11:36:30 -0400

    rmd to tidy, cafe + restaurant data

--- poptimes.py
+++ poptimes.py
@@ -1,14 +1,13 @@
 import populartimes
 import pandas as pd
+import json
 
 key = "AIzaSyC49nDgrH1XV_3GWPZejjfCVizSB7wzZFY"
 
-# print(populartimes.get(key, ["bar"], (44.489066,-73.194732), (44.457708,-73.228400)))
+place = 'cafe'
 
+times = (populartimes.get(key, [place], (44.489066,-73.194732), (44.457708,-73.228400)))
 
-times = [0] * 24
-
-
-bars = pd.read_json("btv-bars.json")
-# nbars = pd.json_normalize(bars)
-print(bars.head())
+filename = "btv-{0}.json".format(place)
+with open(filename, 'w') as fout:
+    json.dump(times , fout)
