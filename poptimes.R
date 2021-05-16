373d4ec2b303f181345a6c56d5dffbdb2b277255
Luke Fredrickson <lukeefredrickson@gmail.com>
2021-05-16 14:13:47 -0400

    tidy dataframes with better place type format

--- poptimes.R
+++ poptimes.R
@@ -1,31 +1,19 @@
----
-title: "populartimes"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
 library(tidyverse)
 library(tidyjson)
 
-```
-
-```{r read}
 read_places_json <- function(place_type) {
   filename <- paste("btv-", place_type, ".json", sep="")
   return(read_json(filename) %>% gather_array)
 }
 places <- c("bar", "cafe", "restaurant")
 places_json <- lapply(places, read_places_json)
-```
 
-```{r tidying}
+
 tidy_places_json <- function(places_json) {
-  spread <- as.data.frame(places_json %>%
-      spread_all %>% 
-      select(array.index, id, name, address)) %>% 
+  tidy_df <- as.data.frame(places_json %>%
+    spread_all %>% 
+    select(array.index, id, name, address)) %>% 
     select(!c(..JSON))
-  print(spread)
   
   times <- places_json %>% 
     enter_object(populartimes) %>% 
@@ -49,27 +37,57 @@ tidy_places_json <- function(places_json) {
     select(!c(..JSON)) %>% 
     pivot_wider(names_from = array.index.2, names_prefix="time.spent.", values_from = value)
   
-  type_names <- c("bar", "supermarket", "church", "gym", "restaurant", "shopping_mall", "store", "department_store")
-  types_df <- data.frame(type_names)
+  type_names <- c("bar", "restaurant", "cafe", "supermarket", "church", "gym", "shopping_mall", "store", "department_store")
+  tidy_df[type_names] <- 0
   
   types <- places_json %>% 
     enter_object(types) %>% 
     gather_array() %>% 
     append_values_string("type") %>% 
     select(!c(document.id))
+  types <- as.data.frame(types)
   
-  types_wide <- as.data.frame(types) %>% 
-    select(!c(..JSON)) %>% 
-    pivot_wider(names_from = array.index.2, names_prefix="type.", values_from = type)
+  rownum <- 0
+  for (i in 1:nrow(types)) {
+    rownum <- types[i, "array.index"]
+    location_num <- types[i, "array.index.2"]
+    type <- types[i, "type"]
+    if (type %in% colnames(tidy_df)) {
+      tidy_df[rownum, type] = location_num
+    }
+  }
   
-  tidy_places <- spread %>% 
-    left_join(types_wide, by="array.index") %>% 
+  tidy_df <- tidy_df %>% 
     left_join(time_spent_wide, by="array.index") %>% 
     left_join(times_wide, by="array.index") %>% select(-c(array.index))
   
+  return(tidy_df)
+}
+
+combine_places <- function(tidy_places_vec) {
+  tidy_places <- as.data.frame(tidy_places_vec[1])
+  for (i in 2:length(tidy_places_vec)) {
+    tidy_places <- union(tidy_places, as.data.frame(tidy_places_vec[i]))
+  }
   return(tidy_places)
 }
 
-tidy_places <- lapply(places_json, tidy_places_json)
-bar <- as.data.frame(tidy_places[1])
-```
+tidy_places_vec <- lapply(places_json, tidy_places_json)
+tidy_places <- combine_places(tidy_places_vec)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- poptimes.py
+++ poptimes.py
@@ -4,9 +4,13 @@ import json
 
 key = "AIzaSyC49nDgrH1XV_3GWPZejjfCVizSB7wzZFY"
 
-place = 'cafe'
+place = "supermarket"
 
-times = (populartimes.get(key, [place], (44.489066,-73.194732), (44.457708,-73.228400)))
+btv_coords_1 = (44.489066,-73.194732)
+btv_coords_2 = (44.457708,-73.228400)
+catamount = (44.44749398413277, -73.04836752811984)
+
+times = (populartimes.get(key, [place], btv_coords_1, btv_coords_2))
 
 filename = "btv-{0}.json".format(place)
 with open(filename, 'w') as fout:
